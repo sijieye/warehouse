85d996782531ec765fa18dccb88b457f41a815c2
sijieye <sijieye@bu.edu>
2023-03-11 23:02:35 -0500

    Adding mui elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Grid';
+import FormControl from '@mui/material/FormControl';
+import { Select, Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={2} style={{ padding: "2rem" }}>
+        <Grid item={true} xs={12} md={12} container alignItems="center" justifyContent="center">
+            <Typography variant="h3" gutterBottom>
+              Summary Report
+            </Typography>
+        </Grid>
+
+        <Grid item={true} xs={12} md={12}>
+          <Typography variant="h5" gutterBottom display="inline-block" margin="10px">
+                Select a shipment ID
+          </Typography>
+          <FormControl style={{minWidth: "20em"}}>
+            <Select fullWidth={true} >
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid  item={true}xs = {12} md={12} margin="1em" >
+          <div>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
