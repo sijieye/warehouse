ee476b7ec819505e8babd3688b1bedf86ba130a7
sijieye <sijieye@bu.edu>
2023-03-13 17:31:31 -0400

    Testing env 4

--- frontend/src/globals.ts
+++ frontend/src/globals.ts
@@ -4,6 +4,7 @@ export const GET_DEFAULT_HEADERS = () => {
     // You will need to add another header here
     headers.append("accept", "application/json");
     headers.append("x-functions-key", process.env.REACT_APP_API_KEY! as string);
+    headers.append("testing", process.env.testing! as string);
     // If you do not, the API will reject your request (:
     return headers;
   };
