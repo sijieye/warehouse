9914950fa3a8af74141cb9f632128a6153fab00d
sijieye <sijieye@bu.edu>
2023-03-13 17:07:42 -0400

    Testing env 2

--- frontend/src/globals.ts
+++ frontend/src/globals.ts
@@ -1,9 +1,11 @@
+import {api_key} from "./index";
+
 export const BASE_API_URL = "https://warehouseautomate.azurewebsites.net/api";
 export const GET_DEFAULT_HEADERS = () => {
     var headers = new Headers();
     // You will need to add another header here
     headers.append("accept", "application/json");
-    headers.append("x-functions-key", process.env.REACT_APP_API_KEY as string);
+    headers.append("x-functions-key", api_key);
     // If you do not, the API will reject your request (:
     return headers;
   };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export const api_key = process.env.REACT_APP_API_KEY as string;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
