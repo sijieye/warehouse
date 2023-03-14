77164cccf20d75486483ced7cd3d1f1cad421b27
sijieye <sijieye@bu.edu>
2023-03-14 18:40:52 -0400

    Updating new azure function 9

--- api/apiKey/index.js
+++ api/apiKey/index.js
@@ -1,8 +1,8 @@
 module.exports = async function (context, req) {
-    const apiKey = process.env.apiKey;
+    // const apiKey = process.env.apiKey;
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: {"apiKey": apiKey}
+        body: {"apiKey": "hello"}
     };
 }
